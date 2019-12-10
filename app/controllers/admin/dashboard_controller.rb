2b1b21390d80cda12dd096232b890818b7b00680
Jay <chunsaa@gmail.com>
2019-12-09 21:19:40 -0500

    display inventory count and category count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: "Jungle", password: "book"
   def show
+    @total_product_count = Product.sum(:quantity)
+    @total_category_count = Product.distinct.count(:category_id)
   end
 end
